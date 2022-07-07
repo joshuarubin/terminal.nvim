0d4cfee06ab3cb1957481cff794407d73489a9a0
Joshua Rubin <me@jawa.dev>
2022-07-07 15:23:17 -0600

    work more better
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- lua/terminal/init.lua
+++ lua/terminal/init.lua
@@ -1,29 +1,35 @@
 local M = {}
 
-M.save = require("terminal.save")
-
 local augroup = vim.api.nvim_create_augroup("terminal.nvim", {})
 
-local function terminal_restore()
-	if vim.bo.buftype ~= "terminal" then
-		return
-	end
-	local mode = vim.w.rubix_term_mode
-	if mode == nil then
+local function terminal_setup(opts)
+	local bufnr = opts.bufnr or 0
+
+	if vim.api.nvim_buf_get_option(bufnr, "buftype") ~= "terminal" then
 		return
 	end
 
-	if mode == "t" then
+	if vim.g.terminal_autoinsert ~= 0 then
 		vim.cmd("startinsert")
 	end
-end
 
-local function terminal_setup()
-	vim.cmd("startinsert")
 	vim.api.nvim_create_autocmd("BufEnter", {
 		desc = "restore terminal mode on enter",
-		buffer = 0,
-		callback = terminal_restore,
+		buffer = bufnr,
+		callback = function()
+			if vim.g.terminal_autoinsert ~= 0 and vim.w.rubix_term_mode == "t" then
+				vim.cmd("startinsert")
+			end
+		end,
+		group = augroup,
+	})
+
+	vim.api.nvim_create_autocmd("BufLeave", {
+		desc = "restore terminal mode on enter",
+		buffer = bufnr,
+		callback = function()
+			vim.w.rubix_term_mode = vim.fn.mode()
+		end,
 		group = augroup,
 	})
 end
