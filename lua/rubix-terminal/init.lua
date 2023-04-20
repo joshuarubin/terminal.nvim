e315741af10306ded4afe8cb7291c4ff46508e3a
Joshua Rubin <me@jawa.dev>
2023-04-19 17:26:05 -0700

    nohlsearch
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- lua/rubix-terminal/init.lua
+++ lua/rubix-terminal/init.lua
@@ -1,24 +1,53 @@
 local M = {}
 
+local settings = {
+	autoinsert = true,
+	nohlsearch = true,
+}
+
 local augroup = vim.api.nvim_create_augroup("terminal.nvim", {})
 
 local function terminal_setup(opts)
 	local bufnr = opts.bufnr or 0
 
-	if vim.api.nvim_buf_get_option(bufnr, "buftype") ~= "terminal" then
+	if bufnr == 0 then
+		bufnr = vim.api.nvim_get_current_buf()
+	end
+
+	if vim.bo[bufnr].buftype ~= "terminal" then
 		return
 	end
 
-	if vim.g.terminal_autoinsert ~= 0 then
+	-- some good default settings for terminals
+	for _, winnr in ipairs(vim.api.nvim_list_wins()) do
+		local buf = vim.api.nvim_win_get_buf(winnr)
+		if buf == bufnr then
+			vim.wo[winnr].number = false
+			vim.wo[winnr].scrolloff = 0
+			vim.wo[winnr].sidescrolloff = 0
+			vim.wo[winnr].signcolumn = "no"
+			vim.wo[winnr].spell = false
+		end
+	end
+
+	if settings.autoinsert then
 		vim.cmd("startinsert")
+	end
+
+	if settings.nohlsearch and vim.o.hlsearch == true then
+		vim.o.hlsearch = false
 	end
 
 	vim.api.nvim_create_autocmd("BufEnter", {
 		desc = "restore terminal mode on enter",
 		buffer = bufnr,
 		callback = function()
-			if vim.g.terminal_autoinsert ~= 0 and vim.w.rubix_term_mode == "t" then
+			if settings.autoinsert and vim.w.rubix_term_mode == "t" then
 				vim.cmd("startinsert")
+			end
+
+			if settings.nohlsearch and vim.o.hlsearch == true then
+				vim.o.hlsearch = false
 			end
 		end,
 		group = augroup,
@@ -29,23 +58,25 @@ local function terminal_setup(opts)
 		buffer = bufnr,
 		callback = function()
 			vim.w.rubix_term_mode = vim.fn.mode()
+			if settings.nohlsearch and vim.o.hlsearch == false then
+				vim.o.hlsearch = true
+			end
 		end,
 		group = augroup,
 	})
 end
 
-M.setup = function()
-	vim.api.nvim_create_autocmd("TermOpen", {
-		desc = "start new terminals in insert mode",
-		pattern = "*",
-		callback = terminal_setup,
-		group = augroup,
-	})
+M.setup = function(opts)
+	opts = opts or {}
+	opts.autoinsert = opts.autoinsert or true
+	opts.nohlsearch = opts.nohlsearch or true
+
+	settings.autoinsert = opts.autoinsert == true
+	settings.nohlsearch = opts.nohlsearch == true
 
 	vim.api.nvim_create_autocmd("TermOpen", {
-		desc = "some good default settings for terminals",
 		pattern = "*",
-		command = "setlocal nonumber nospell signcolumn=no sidescrolloff=0 scrolloff=0",
+		callback = terminal_setup,
 		group = augroup,
 	})
 end
